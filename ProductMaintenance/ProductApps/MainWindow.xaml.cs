3c43c0eebf20f7c10cb5554503c32fa3674449cf
THANH HAO LUONG <thanh.luong3@student.tafesa.edu.au>
2023-09-15 11:28:43 +0930

    added the delivery charge $25

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace ProductApps
     public partial class MainWindow : Window
     {
         Product cProduct;
+        decimal totalCharge;
 
         public MainWindow()
         {
@@ -34,6 +35,8 @@ namespace ProductApps
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+                totalCharge = cProduct.TotalPayment + 25;
+                totalChargeTextBox.Text = Convert.ToString(totalCharge);
             }
             catch (FormatException)
             {
@@ -47,6 +50,8 @@ namespace ProductApps
             priceTextBox.Text = "";
             quantityTextBox.Text = "";
             totalPaymentTextBlock.Text = "";
+            totalPaymentTextBlock.Text = "";
+            totalChargeTextBox.Text = "";
         }
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
